3b8aff5166048c0a6c2e47c1721ca8f31dca4937
sohail <pro20.08.faizan@gmail.com>
2021-04-09 13:37:44 +0000

    validation of user input

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.use(bodyParser.json());
 app.post('/',(req,res,next)=>{
   amount = req.body.amount;
   email = req.body.email;
+  if(amount<=1)
+  {
+    return_info = {};
+    return_info.error = true;
+    return_info.message = "The amount should be greater than 1";
+    return res.send(return_info);
+  }
+
   res.send({"amount":amount,"email":email,"message":"Success!"});
 });
 

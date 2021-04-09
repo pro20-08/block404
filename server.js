2fcf4326df2d4d1bba6ed053afa0cd0e93e99659
sohail <pro20.08.faizan@gmail.com>
2021-04-09 12:57:59 +0000

    Postman setup test

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.post('/',(req,res,next)=>{
   amount = req.body.amount;
   email = req.body.email;
-  res.send({"amount":amount,"email":email});
+  res.send({"amount":amount,"email":email,"message":"Success!"});
 });
 
 app.listen('3000',()=>{

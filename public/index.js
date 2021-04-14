50cf179b23ac7b5fe8f9995c2213483bf5130840
sohail <pro20.08.faizan@gmail.com>
2021-04-14 12:37:37 +0000

    React Setup

---
+++ public/index.js
@@ -0,0 +1,15 @@
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+            <h1>Lottery Application 2.0</h1>
+      </div>)
+  }
+}
+
+ReactDOM.render(
+  <div>
+  <App />
+  </div> ,
+  document.getElementById('reactBinding')
+);
